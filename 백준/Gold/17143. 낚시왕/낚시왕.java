7e70d097e1436de1d7345495b3f449846482ddf3
cmsik1 <138847274+cmsik1@users.noreply.github.com>
2026-03-10 00:09:32 +0900

    [Gold I] Title: 낚시왕, Time: 592 ms, Memory: 22884 KB -BaekjoonHub

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -3,124 +3,103 @@ import java.io.*;
 
 public class Main {
 
-	static int R, C, M;
-	static int[][] map, sharks;
+    static int R, C, M;
+    static int[] dr = {0, -1, 1, 0, 0};
+    static int[] dc = {0, 0, 0, 1, -1};
+    static int[] revDir = {0, 2, 1, 4, 3};
+    static Shark[] sharks;
+    static Shark[][] map;
 
-	static int getTotalSum() {
-		int totalSum = 0;
+    static int catchShark() {
+        int count = 0;
 
-		for (int c = 0; c < C; c++) {
-			for (int r = 0; r < R; r++) {
-				if (map[r][c] != 0) {
-					totalSum += sharks[map[r][c]][4];
-					sharks[map[r][c]][5] = 0;
-					map[r][c] = 0;
-					break;
-				}
-			}
+        for (int fisher = 0; fisher < C; fisher++) {
+            for (int r = 0; r < R; r++) {
+                if (map[r][fisher] != null) {
+                    count += map[r][fisher].size;
+                    map[r][fisher].isAlive = false;
+                    map[r][fisher] = null;
+                    break;
+                }
+            }
 
-			for (int i = 1; i <= M; i++) {
-				int[] shark = sharks[i];
-				if (shark[5] == 1) { // 살아있는 상어이면
-					map[shark[0]][shark[1]] = 0;
+            for (int s = 0; s < M; s++) {
+                if (sharks[s].isAlive) sharks[s].move();
+            }
 
-					boolean isIncrease = (shark[3] == 2 || shark[3] == 3) ? true : false;
-					boolean isRow = (shark[3] == 1 || shark[3] == 2) ? false : true;
-					int[] newPos = moveShark(shark[0], shark[1], shark[2], isIncrease, isRow);
-					shark[0] = newPos[0];
-					shark[1] = newPos[1];
-					shark[3] = newPos[2];
-				}
-			}
+            for (int s = 0; s < M; s++) {
+                if (sharks[s].isAlive) {
+                    if (map[sharks[s].row][sharks[s].col] == null) {
+                        map[sharks[s].row][sharks[s].col] = sharks[s];
+                    } else {
+                        if (map[sharks[s].row][sharks[s].col].size > sharks[s].size) {
+                            sharks[s].isAlive = false;
+                        } else {
+                            map[sharks[s].row][sharks[s].col].isAlive = false;
+                            map[sharks[s].row][sharks[s].col] = sharks[s];
+                        }
+                    }
+                }
+            }
+        }
 
-			for (int i = 1; i <= M; i++) {
-				int[] shark = sharks[i];
-				if (shark[5] == 1) {
-					if (map[shark[0]][shark[1]] == 0) {
-						map[shark[0]][shark[1]] = i;
-					} else if (sharks[map[shark[0]][shark[1]]][4] < shark[4]) {
-						sharks[map[shark[0]][shark[1]]][5] = 0;
-						map[shark[0]][shark[1]] = i;
-					} else {
-						sharks[i][5] = 0;
-					}
-				}
-			}
-		}
+        return count;
+    }
 
-		return totalSum;
-	}
 
-	/*
-	 * 현재 좌표, 행 or 열 길이(lastIdx), 증감여부, 행/열여부 이동 이후 좌표만 반환
-	 */
-	static int[] moveShark(int cr, int cc, int speed, boolean isIncrease, boolean isRow) {
-		int flag = 0;
-		int initMove = 0;
-		if (isRow) {
-			initMove = (isIncrease) ? (C - 1) - cc : cc;
-		} else {
-			initMove = (isIncrease) ? (R - 1) - cr : cr;
-		}
+    static class Shark {
+        int row, col, speed, dir, size;
+        boolean isAlive;
 
-		if (speed <= initMove) {
-			if (isRow) {
-				cc = (isIncrease) ? cc + speed : cc - speed;
-			} else {
-				cr = (isIncrease) ? cr + speed : cr - speed;
-			}
-			flag = 1;
-		}
+        Shark(int row, int col, int speed, int dir, int size, boolean isAlive) {
+            this.row = row;
+            this.col = col;
+            this.speed = speed;
+            this.dir = dir;
+            this.size = size;
+            this.isAlive = isAlive;
+        }
 
-		if (flag == 0) {
-			speed -= initMove;
-			if (isRow) {
-				if ((speed / (C - 1)) % 2 == 0) {
-					isIncrease = (isIncrease) ? false : true;
-				}
-				cc = (isIncrease) ? (speed % (C - 1)) : (C - 1) - (speed % (C - 1));
-			} else {
-				if ((speed / (R - 1)) % 2 == 0) {
-					isIncrease = (isIncrease) ? false : true;
-				}
-				cr = (isIncrease) ? (speed % (R - 1)) : (R - 1) - (speed % (R - 1));
-			}
-		}
+        void move() {
+            map[row][col] = null;
+            int totalMove = 0;
 
-		int dir = 0;
-		if (!isRow) {
-			dir = (isIncrease) ? 2 : 1;
-		} else {
-			dir = (isIncrease) ? 3 : 4;
-		}
+            if (dir == 1 || dir == 2) totalMove = speed % ((R - 1) * 2);
+            else totalMove = speed % ((C - 1) * 2);
 
-		return new int[] { cr, cc, dir };
-	}
+            for (int i = 0; i < totalMove; i++) {
+                int nr = row + dr[dir];
+                int nc = col + dc[dir];
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+                if (nr < 0 || nr >= R || nc < 0 || nc >= C) dir = revDir[dir];
 
-		R = Integer.parseInt(st.nextToken());
-		C = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		map = new int[R][C];
-		sharks = new int[M + 1][6];
+                row += dr[dir];
+                col += dc[dir];
+            }
+        }
+    }
 
-		for (int i = 1; i <= M; i++) {
-			st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-			sharks[i][0] = Integer.parseInt(st.nextToken()) - 1;
-			sharks[i][1] = Integer.parseInt(st.nextToken()) - 1;
-			sharks[i][2] = Integer.parseInt(st.nextToken());
-			sharks[i][3] = Integer.parseInt(st.nextToken());
-			sharks[i][4] = Integer.parseInt(st.nextToken());
-			sharks[i][5] = 1;
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new Shark[R][C];
+        sharks = new Shark[M];
 
-			map[sharks[i][0]][sharks[i][1]] = i;
-		}
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
 
-		System.out.println(new StringBuilder().append(getTotalSum()));
-	}
+            sharks[i] = new Shark(Integer.parseInt(st.nextToken()) - 1, Integer.parseInt(st.nextToken()) - 1,
+                Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), true);
+            map[sharks[i].row][sharks[i].col] = sharks[i];
+        }
+
+        System.out.println(sb.append(catchShark()).toString());
+    }
 
 }
+
