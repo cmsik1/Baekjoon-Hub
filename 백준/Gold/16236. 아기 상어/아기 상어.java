b8ca87ac1778788d811aa080676bc1c51d80b4aa
cmsik1 <138847274+cmsik1@users.noreply.github.com>
2026-03-10 22:59:49 +0900

    [Gold III] Title: 아기 상어, Time: 116 ms, Memory: 14756 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -2,80 +2,76 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
     static int N, cr, cc, size, caughtFish;
-    static int[] dr = {-1, 0, 0, 1};
-    static int[] dc = {0, -1, 1, 0};
+    static int[] dr = {-1, 0, 1, 0};
+    static int[] dc = {0, -1, 0, 1};
     static int[][] map;
-    static boolean[][] visited;
+
+    // 물고기/상어 위치 정보를 담는 클래스 (가독성 핵심)
+    static class Node implements Comparable<Node> {
+        int r, c, dist;
+        Node(int r, int c, int dist) {
+            this.r = r; this.c = c; this.dist = dist;
+        }
+
+        // 문제의 우선순위 조건을 여기에 정의 (거리 -> 행 -> 열)
+        @Override
+        public int compareTo(Node o) {
+            if (this.dist != o.dist) return this.dist - o.dist;
+            if (this.r != o.r) return this.r - o.r;
+            return this.c - o.c;
+        }
+    }
 
     static int simulation() {
         int totalTime = 0;
-        caughtFish = 0;
-        size = 2;
+        size = 2; caughtFish = 0;
 
         while (true) {
-            int time = bfs();
-            if (time == 0) {
-                return totalTime;
-            }
+            Node target = findTarget(); // BFS 로직 분리
+            if (target == null) return totalTime;
 
-            totalTime += time;
-            caughtFish++;
+            // 상어 상태 갱신
+            totalTime += target.dist;
+            map[target.r][target.c] = 0;
+            cr = target.r;
+            cc = target.c;
 
-            if (caughtFish == size) {
+            if (++caughtFish == size) {
                 size++;
                 caughtFish = 0;
             }
         }
     }
 
-    static int bfs() {
-        ArrayDeque<int[]> dq = new ArrayDeque<>();
-        dq.add(new int[]{cr, cc, 0});
-        visited = new boolean[N][N];
+    static Node findTarget() {
+        PriorityQueue<Node> pq = new PriorityQueue<>(); // 거리와 위치를 동시에 고려
+        pq.add(new Node(cr, cc, 0));
+        boolean[][] visited = new boolean[N][N];
         visited[cr][cc] = true;
-        List<int[]> target = new ArrayList<>();
-        int flag = 0;
 
-        while (!dq.isEmpty()) {
-            int[] pos = dq.poll();
-            int r = pos[0];
-            int c = pos[1];
-            int time = pos[2];
+        while (!pq.isEmpty()) {
+            Node curr = pq.poll();
 
-            if (flag == 1 && target.get(0)[2] != time) {
-                Collections.sort(target, (a, b) -> {
-                    if (a[0] == b[0]) {
-                        return a[1] - b[1];
-                    } else {
-                        return a[0] - b[0];
-                    }
-                });
-
-                int[] next = target.get(0);
-                cr = next[0];
-                cc = next[1];
-                map[cr][cc] = 0;
-                return next[2] + 1;
+            // 먹을 수 있는 물고기를 찾았다면?
+            // PriorityQueue이므로 가장 가까운 거리 중 상단/좌측 물고기가 먼저 나옴
+            if (map[curr.r][curr.c] > 0 && map[curr.r][curr.c] < size) {
+                return curr;
             }
 
             for (int i = 0; i < 4; i++) {
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+                int nr = curr.r + dr[i];
+                int nc = curr.c + dc[i];
 
-                if (0 <= nr && nr < N && 0 <= nc && nc < N && !visited[nr][nc] && map[nr][nc] <= size) {
-                    if (map[nr][nc] > 0 && map[nr][nc] < size) {
-                        target.add(new int[]{nr, nc, time});
-                        flag = 1;
-                    }
+                if (nr < 0 || nr >= N || nc < 0 || nc >= N || visited[nr][nc]) continue;
+
+                if (map[nr][nc] <= size) {
                     visited[nr][nc] = true;
-                    dq.add(new int[]{nr, nc, time + 1});
+                    pq.add(new Node(nr, nc, curr.dist + 1));
                 }
             }
         }
-
-        return 0;
+        return null;
     }
 
     public static void main(String[] args) throws IOException {
@@ -86,17 +82,13 @@ public class Main {
         for (int r = 0; r < N; r++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int c = 0; c < N; c++) {
-                int value = Integer.parseInt(st.nextToken());
-                map[r][c] = value;
-                if (value == 9) {
-                    cr = r;
-                    cc = c;
+                map[r][c] = Integer.parseInt(st.nextToken());
+                if (map[r][c] == 9) {
+                    cr = r; cc = c;
                     map[r][c] = 0;
                 }
             }
         }
-
-        System.out.println(new StringBuffer().append(simulation()).toString());
+        System.out.println(simulation());
     }
-
 }
