0d84f622593d6a3d76f39b94b65f162d39339898
cmsik1 <138847274+cmsik1@users.noreply.github.com>
2026-04-07 23:01:43 +0900

    [Gold II] Title: 빵집, Time: 356 ms, Memory: 33760 KB -BaekjoonHub

---
+++ 백준/Gold/3109. 빵집/빵집.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static int R, C;
+    static int[] dr = {-1, 0, 1};
+    static char[][] map;
+
+    static int getMaxPipeline() {
+        int count = 0;
+
+        for (int r = 0; r < R; r++) {
+            if (setPipeline(r, 0) == 'X') {
+                count++;
+            }
+        }
+
+        return count;
+    }
+
+    static char setPipeline(int r, int c) {
+        map[r][c] = 'X';
+
+        if (c == C - 1) {
+            return 'X';
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int nr = r + dr[i];
+            int nc = c + 1;
+
+            if (0 <= nr && nr < R && map[nr][nc] == '.') {
+                if (setPipeline(nr, nc) == 'X') {
+                    return 'X';
+                }
+            }
+        }
+
+        return '.';
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        R = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
+        map = new char[R][C];
+
+        for (int r = 0; r < R; r++) {
+            String input = br.readLine();
+            for (int c = 0; c < C; c++) {
+                map[r][c] = input.charAt(c);
+            }
+        }
+
+        System.out.println(getMaxPipeline());
+    }
+}
